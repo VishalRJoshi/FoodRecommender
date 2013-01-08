3111d49f54b1caed18a37e23ce830cf036a743a3
Nikhil Kothari <nikhilk@live.com>
2013-01-08 15:46:38 -0800

    Basic implementation for recording food votes

---
+++ Scripts/Site.js
@@ -0,0 +1,10 @@
+﻿function savor(button, id, title) {
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState == 4) {
+      alert('Your preference for ' + title + ' has been recorded. Thanks!');
+    }
+  }
+  xhr.open('POST', 'Savor?recipe=' + id, true);
+  xhr.send();
+}
